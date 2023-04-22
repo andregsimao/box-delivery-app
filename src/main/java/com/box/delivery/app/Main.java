af0255a4b801ee3ae12dfe619a10c6611f82bbf3
andregsimao <andre.simao@gruposbf.com.br>
2023-04-21 23:22:00 -0400

    feat: add hibernate persistence in Main class

--- src/main/java/com/box/delivery/app/Main.java
+++ src/main/java/com/box/delivery/app/Main.java
@@ -1,22 +1,43 @@
 package com.box.delivery.app;
 
+import com.box.delivery.app.config.HibernateUtil;
+import com.box.delivery.app.entity.Airport;
+import com.box.delivery.app.entity.Flight;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        Airport departure = new Airport("YYC", "Calgary");
+        Airport arrival = new Airport("GIG", "Galeao");
+        Flight flight = new Flight(1, 2, departure, arrival);
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // save the student objects
+            session.persist(flight);
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
 
-        System.out.println("Enter a string:");
-        String s = in.nextLine();
-        System.out.println("You entered string " + s);
-
-        System.out.println("Enter an integer:");
-        int a = in.nextInt();
-        System.out.println("You entered integer " + a);
-
-        System.out.println("Enter a float:");
-        float b = in.nextFloat();
-        System.out.println("You entered float " + b);
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            List< Airport > airports = session.createQuery("from Airport", Airport.class).list();
+            airports.forEach(a -> System.out.println(a.getCode()));
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 }
 
