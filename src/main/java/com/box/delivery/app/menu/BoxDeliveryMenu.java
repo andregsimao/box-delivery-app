51302e860ebc01ffee9d1d11a43e9d568ca8507a
andregsimao <andre.simao@gruposbf.com.br>
2023-04-22 13:43:58 -0400

    feat: add scanner menu logic

--- src/main/java/com/box/delivery/app/menu/BoxDeliveryMenu.java
+++ src/main/java/com/box/delivery/app/menu/BoxDeliveryMenu.java
@@ -1,11 +1,11 @@
 package com.box.delivery.app.menu;
 
-import com.box.delivery.app.Application;
 import com.box.delivery.app.Repository.FlightRepository;
 import com.box.delivery.app.config.HibernateUtil;
 import com.box.delivery.app.entity.Airport;
 import com.box.delivery.app.entity.Flight;
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +19,22 @@ public class BoxDeliveryMenu {
     }
 
     public void run() {
+        showMainMenu();
+        CommandOption commandOption = getChosenOption();
+        while (commandOption != CommandOption.EXIT) {
+            switch (commandOption) {
+                case EXIT:
+                    return;
+                case INSERT_FLIGHTS:
+                    break;
+                case CHECK_FLIGHTS:
+                    break;
+                case GENERATE_ITINERARIES:
+                    break;
+            }
+            commandOption = getChosenOption();
+        }
+
         FlightRepository flightRepository = FlightRepository.getInstance();
         Airport departure = new Airport("YYC", "Calgary");
         Airport arrival = new Airport("GIG", "Galeao");
@@ -39,7 +55,34 @@ public class BoxDeliveryMenu {
         logger.info("scanner closed");
     }
 
-    public void showMainMenu() {
+    private void showMainMenu() {
+        printMessage("Option between 0 and 3");
+    }
 
+    private CommandOption getChosenOption() {
+        while(scanner.hasNext()){
+            if(scanner.hasNextInt(10)){
+                int optionValue = scanner.nextInt();
+                Optional<CommandOption> commandOption = CommandOption.getOptionByValue(optionValue);
+                if(commandOption.isPresent()) {
+                    return commandOption.get();
+                } else {
+                    printMessage("Invalid option " + optionValue + ". Choose between the options in the menu");
+                }
+            } else{
+                printMessage("The option needs to be an Integer number");
+                scanner.next();
+            }
+            skipSpecialChars();
+        }
+        return CommandOption.EXIT;
+    }
+
+    private void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    private void skipSpecialChars() {
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
     }
 }
---
+++ src/main/java/com/box/delivery/app/menu/CommandOption.java
@@ -0,0 +1,28 @@
+package com.box.delivery.app.menu;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public enum CommandOption {
+    EXIT(0, "Finish the program"),
+    INSERT_FLIGHTS(1, "Insert flight schedule"),
+    CHECK_FLIGHTS(2, "Check flight schedule"),
+    GENERATE_ITINERARIES(3, "Generate itineraries");
+
+    final int value;
+    final String description;
+
+    CommandOption(int value, String description) {
+        this.value = value;
+        this.description = description;
+    }
+
+    static Optional<CommandOption> getOptionByValue(int optionValue) {
+        for(CommandOption commandOption: CommandOption.values()) {
+            if(commandOption.value == optionValue) {
+                return Optional.of(commandOption);
+            }
+        }
+        return Optional.empty();
+    }
+}
